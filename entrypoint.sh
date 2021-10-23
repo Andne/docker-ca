ac8beda391e79eced848c6d771feff28f1a005ab
Andy Neebel <andy@andne.net>
2021-10-23 13:30:47 -0500

    Improvements for shutdown, export cert bundle

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,30 +8,56 @@ mkdir -p /var/lib/rootca/output
 
 touch /var/lib/rootca/index.txt
 
+new_files_generated=no
+
 # Generate a new root key if one hasn't been generated already
 if [ ! -e /var/lib/rootca/local/rootCA.key ] ; then
+    echo "No root key found, generating new root key"
     openssl genpkey -algorithm ED25519 -outform PEM -out /var/lib/rootca/local/rootCA.key
     chmod 400 /var/lib/rootca/local/rootCA.key
+
+    new_files_generated=yes
 fi
 
 # Generate a new root certificate if one hasn't been generated already
 if [ ! -e /var/lib/rootca/local/rootCA.pem ] ; then
+    echo "No root certificate found, generating new root certificate"
     (
         export subjectAltName="email:certs@tridigee.com"
         openssl req -x509 -config /etc/rootca.cnf -new -nodes -sha256 -days 3650 \
             -subj "/C=US/ST=IA/O=Andne.net/CN=Andne Root CA" \
             -key /var/lib/rootca/local/rootCA.key -out /var/lib/rootca/local/rootCA.pem
     )
-    chmod 400 /var/lib/rootca/local/rootCA.pem
+    chmod 444 /var/lib/rootca/local/rootCA.pem
+
+    new_files_generated=yes
+fi
+
+# Export an archive if any new files were created
+if [ "${new_files_generated}" == "yes" ] ; then
+    echo "Exporting root certificate for archiving"
+    tar cf /var/lib/rootca/output/Andne-Root-Cert.tar -C /var/lib/rootca/local rootCA.key rootCA.pem
+    chmod 440 /var/lib/rootca/output/Andne-Root-Cert.tar
 fi
 
 # Make sure the certificate can be downloaded as wanted
 if [ ! -e /var/lib/rootca/output/rootCA.pem ] ; then
     cp /var/lib/rootca/local/rootCA.pem /var/lib/rootca/output/rootCA.pem
-    chmod 444 /var/lib/rootca/output/rootCA.pem
 fi
 
 openssl x509 -in /var/lib/rootca/local/rootCA.pem -text
+
+
+#############################################################
+## Logic to keep running and properly shut down
+#############################################################
+
+Shutdown() {
+    keep_running=0
+    exit 0
+}
+
+trap Shutdown SIGINT SIGTERM
 
 while : ; do
     sleep 1
